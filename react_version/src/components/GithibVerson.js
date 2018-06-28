5b383c6587b061445929198a1946760dc73ca1a1
tejpal sohal <tejpal54@gmail.com>
2018-06-28 15:34:40 +0100

    Git version in console.log

--- react_version/src/components/GithibVerson.js
+++ react_version/src/components/GithibVerson.js
@@ -1,46 +1,47 @@
 import React, { Component } from 'react';
 import $ from 'jquery/dist/jquery';
 
-const green = { color:'#fff', textAlign:'left' } 
-const PullLeft = {textAlign:'left' }
+const green = { color: '#fff', textAlign: 'left' }
+const PullLeft = { textAlign: 'left' }
 
 class GithubVersion extends Component {
     constructor(props) {
-       super()
-       this.state = {
-           github : ''
-       }
-    //    this.GetData();
+        super()
+        this.state = {
+            github: []
+        }
+    }
+
+    componentDidMount() {
+        this.GetData();
 
     }
 
-    GetData(){
-        fetch('https://github.com/pixel54/pixel54.github.io/commits/master.atom')
-        .then(data => data.text())
-        .then((data) => {
-            let str = data
-            let xmlDoc = $.parseXML(str)
-            let $xml = $(xmlDoc)
-            let $title = $xml.find("id")
-            this.setState({ github: $title[1]})
-        })
+
+    GetData() {
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "https://github.com/pixel54/pixel54.github.io/commits/master.atom"; // site that doesn’t send Access-Control-*
+        fetch(proxyurl + url)
+            .then(data => data.text())
+            .then((data) => {
+                let str = data
+                let xmlDoc = $.parseXML(str)
+                let $xml = $(xmlDoc)
+                let $title = $xml.find("id")
+                this.setState({ github: [...$title] })
+
+            })
+            .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 
     }
- 
-    
-
 
 
     render() {
-        // console.log(this.state.github)
-        return(
+        console.log(this.state.github[1])
+        return (
             <div style={PullLeft}>
-<<<<<<< Updated upstream
-                {/* <p style={green}>{this.state.github}</p> */}
-=======
-                <p style={green}>{this.state}</p>
->>>>>>> Stashed changes
-            </div>    
+                <div id="gitcommit"></div>
+            </div>
         )
     }
 
